bc3bc9874b57509652f64aefa32f86917e1fb285
Ayushm30 <134107707+Ayushm30@users.noreply.github.com>
2024-06-16 23:09:14 +0000

    comparison

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+console.log(typeof (score));
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isloggedIn = ""
+
+let booleanIsLogged = Boolean(isloggedIn)
+console.log(booleanIsLogged );
+// 1 =>true 0 =>false
+
+// ********************** OPERATIONS **************************
+
+let str1 = "ayush";
+let str2 = "Chaudhary";
+
+let str3 = str1 +str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let game =100;
+++game;
+console.log(game);
