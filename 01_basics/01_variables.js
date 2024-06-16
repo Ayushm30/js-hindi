d5d152860d7432f4fb57bad2a13354ad7061ea70
Ayushm30 <134107707+Ayushm30@users.noreply.github.com>
2024-06-16 19:47:31 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let accountEmail = "ayushm23y@gmail.com";
+accountCity = "Delhi";
+let accountname;
+/*
+always use let and never use var
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountCity]);
+console.table([accountId, accountEmail,accountCity,accountname]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+console.log(3+3);
+console.log("Ayush");
+
+let name = "Ayush"
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+//numsber => 2 to power 53
+//bigint
+//string => ""
+//boolean = true/false
+//null = standalone value
+//undefined 
+//symbol = unique
+
+console.log(typeof "Ayush");
+console.log(typeof null);
+console.log(typeof undefined);
