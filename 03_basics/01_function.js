49d945447e14feb22b02f550f665f3258bc4dd30
Ayushm30 <134107707+Ayushm30@users.noreply.github.com>
2024-06-21 21:31:17 +0000

    function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -6,3 +6,26 @@ function saymyname(){
     console.log("H")
 }
 saymyname()
+
+//function addtwonumber(number1,number2){
+//    console.log(number1+number2)
+//}
+//addtwonumber(10,20)
+
+function addtwonumber(number1,number2){
+    //let result = number1 + number2
+    //return result;
+    return number1 + number2
+}
+const result = addtwonumber(3,5)
+console.log("Result :",result);
+addtwonumber(10,20)
+
+function loginUserMessage(username){
+    if(username === undefined ){
+        console.log("please enter a usernname");
+        return 
+    }
+    return  `${username} just logged in`
+}
+console.log(loginUserMessage())
